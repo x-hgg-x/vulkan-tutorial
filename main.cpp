a12d83e55bd8c0d9199b3f6585fff903ecf3bb64
x-hgg-x <39058530+x-hgg-x@users.noreply.github.com>
2019-05-08 19:19:23 +0200

    Load models

--- main.cpp
+++ main.cpp
@@ -4,11 +4,17 @@
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#define GLM_ENABLE_EXPERIMENTAL
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/hash.hpp>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
+
+#define TINYOBJLOADER_IMPLEMENTATION
+#include "tiny_obj_loader.h"
 
 #include <algorithm>
 #include <chrono>
@@ -20,9 +26,13 @@
 #include <set>
 #include <stddef.h>
 #include <stdexcept>
+#include <unordered_map>
 
 const int WIDTH = 800;
 const int HEIGHT = 600;
+
+const std::string MODEL_PATH = "models/chalet.obj";
+const std::string TEXTURE_PATH = "textures/chalet.jpg";
 
 const int MAX_FRAMES_IN_FLIGHT = 1;
 
@@ -53,19 +63,20 @@ struct Vertex {
                 {1, 0, vk::Format::eR32G32B32Sfloat, offsetof(Vertex, color)},
                 {2, 0, vk::Format::eR32G32Sfloat, offsetof(Vertex, texCoord)}};
     }
+
+    bool operator==(const Vertex &other) const
+    {
+        return pos == other.pos && color == other.color && texCoord == other.texCoord;
+    }
 };
 
-const std::vector<Vertex> vertices = {
-    {{-0.5, -0.5, 0}, {1, 0, 0}, {1, 0}},
-    {{0.5, -0.5, 0}, {0, 1, 0}, {0, 0}},
-    {{0.5, 0.5, 0}, {0, 0, 1}, {0, 1}},
-    {{-0.5, 0.5, 0}, {1, 1, 1}, {1, 1}},
-    {{-0.5, -0.5, -0.5}, {1, 0, 0}, {1, 0}},
-    {{0.5, -0.5, -0.5}, {0, 1, 0}, {0, 0}},
-    {{0.5, 0.5, -0.5}, {0, 0, 1}, {0, 1}},
-    {{-0.5, 0.5, -0.5}, {1, 1, 1}, {1, 1}}};
-
-const std::vector<uint16_t> indices = {0, 1, 2, 2, 3, 0, 4, 5, 6, 6, 7, 4};
+template <>
+struct std::hash<Vertex> {
+    size_t operator()(Vertex const &vertex) const
+    {
+        return ((std::hash<glm::vec3>()(vertex.pos) ^ (std::hash<glm::vec3>()(vertex.color) << 1)) >> 1) ^ (std::hash<glm::vec2>()(vertex.texCoord) << 1);
+    }
+};
 
 struct UniformBufferObject {
     alignas(16) glm::mat4 model;
@@ -124,6 +135,9 @@ class HelloVulkan
     std::vector<vk::UniquePipeline> graphicsPipelines;
 
     vk::UniqueCommandPool commandPool;
+
+    std::vector<Vertex> vertices;
+    std::vector<uint32_t> indices;
 
     vk::UniqueDeviceMemory depthImageMemory;
     vk::UniqueDeviceMemory textureImageMemory;
@@ -188,6 +202,7 @@ class HelloVulkan
         createTextureImage();
         createTextureImageView();
         createTextureSampler();
+        loadModel();
         createVertexBuffer();
         createIndexBuffer();
         createUniformBuffers();
@@ -326,7 +341,7 @@ class HelloVulkan
             commandBuffers[i].beginRenderPass(&renderPassInfo, vk::SubpassContents::eInline);
             commandBuffers[i].bindPipeline(vk::PipelineBindPoint::eGraphics, *graphicsPipelines[0]);
             commandBuffers[i].bindVertexBuffers(0, *vertexBuffer, {0});
-            commandBuffers[i].bindIndexBuffer(*indexBuffer, 0, vk::IndexType::eUint16);
+            commandBuffers[i].bindIndexBuffer(*indexBuffer, 0, vk::IndexType::eUint32);
             commandBuffers[i].bindDescriptorSets(vk::PipelineBindPoint::eGraphics, *pipelineLayout, 0, descriptorSets[i], {});
             commandBuffers[i].drawIndexed(indices.size(), 1, 0, 0, 0);
             commandBuffers[i].endRenderPass();
@@ -474,7 +489,7 @@ class HelloVulkan
     void createTextureImage()
     {
         int texWidth, texHeight, texChannels;
-        stbi_uc *pixels = stbi_load("textures/texture.jpg", &texWidth, &texHeight, &texChannels, STBI_rgb_alpha);
+        stbi_uc *pixels = stbi_load(TEXTURE_PATH.c_str(), &texWidth, &texHeight, &texChannels, STBI_rgb_alpha);
         if (!pixels) {
             throw std::runtime_error("failed to load texture image!");
         }
@@ -628,6 +643,36 @@ class HelloVulkan
 
             // vk::FramebufferCreateInfo(flags_, renderPass_, attachmentCount_, pAttachments_, width_, height_, layers_)
             swapChainFramebuffers[i] = device->createFramebufferUnique({{}, *renderPass, (uint32_t)attachments.size(), attachments.data(), swapChainExtent.width, swapChainExtent.height, 1});
+        }
+    }
+
+    void loadModel()
+    {
+        tinyobj::attrib_t attrib;
+        std::vector<tinyobj::shape_t> shapes;
+        std::vector<tinyobj::material_t> materials;
+        std::string warn, err;
+
+        if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, MODEL_PATH.c_str())) {
+            throw std::runtime_error(warn + err);
+        }
+
+        std::unordered_map<Vertex, uint32_t> uniqueVertices;
+        for (const auto &shape : shapes) {
+            for (const auto &index : shape.mesh.indices) {
+                Vertex vertex = {
+                    glm::make_vec3(&attrib.vertices[3 * index.vertex_index]),
+                    {1, 1, 1},
+                    glm::make_vec2(&attrib.texcoords[2 * index.texcoord_index])};
+
+                vertex.texCoord[1] = 1 - vertex.texCoord[1];
+
+                if (uniqueVertices.count(vertex) == 0) {
+                    uniqueVertices[vertex] = vertices.size();
+                    vertices.push_back(vertex);
+                }
+                indices.push_back(uniqueVertices[vertex]);
+            }
         }
     }
 
